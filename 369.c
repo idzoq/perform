fe8efe83d4771fdfb10849c77c2bad9e0b43a9bb
idzoq <idzoq@naver.com>
2015-05-28 19:49:08 +0900

    369
    It just 369s.

---
+++ 369.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <conio.h>
+
+#pragma warning (disable : 4996)
+
+int main(void)
+{
+	int max;
+
+	char str[10];
+
+	int i, j, k, five = 0;
+
+	int jjak = 0;
+
+	printf("MAX 값을 설정하세요 : ");
+	scanf("%d", &max);
+	puts("");
+
+	for (i = 1; i <= max; i++) {
+		itoa(i, str, 10);
+
+		j = 0;
+		while (str[j] != NULL) {
+			if (str[j] == '3' || str[j] == '6' || str[j] == '9') {
+				jjak++;
+			}
+			j++;
+		}
+
+		if (jjak > 0) {
+			for (k = 0; k < jjak; k++) {
+				printf("짝");
+			}
+			printf("\t");
+		}
+		else {
+			printf("%d\t", i);
+		}
+		jjak = 0;
+
+
+		five++;
+
+		if (five == 5) {
+			puts("");
+			puts("- - - - - - - - - - - - - - - - - - -");
+			five = 0;
+		}
+	}
+
+	_getch();
+	return 0;
+}
